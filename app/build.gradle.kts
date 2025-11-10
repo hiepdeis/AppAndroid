e8b27d9fd1a51a737679cee21b0c20ea1ae97b36
Mr.Warren <194219818+hiepdeis@users.noreply.github.com>
2025-11-10 22:37:29 +0700

    yu

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     id("com.google.gms.google-services")
+
 }
 
 android {
@@ -46,9 +47,14 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+    implementation("com.cloudinary:cloudinary-android:2.4.0")
 
+    // Thư viện Glide để load ảnh
+    implementation("com.github.bumptech.glide:glide:4.16.0")
     // Firebase dependencies
     implementation(platform("com.google.firebase:firebase-bom:32.7.0"))
     implementation("com.google.firebase:firebase-auth")
     implementation("com.google.firebase:firebase-analytics")
+
+    implementation("com.kizitonwose.calendar:view:2.6.0")
 }
